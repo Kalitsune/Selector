80968be5ed60bd0c21d22c377fa7dbcf19985230
kalitsune <kalithekitsune@gmail.com>
2022-09-16 18:58:16 +0200

    Api rework (for better comprehension)

--- backend/handler/lists.go
+++ backend/handler/lists.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"bytes"
-	"encoding/json"
 	"github.com/gofiber/fiber/v2"
 	"github.com/kalitsune/selector/api"
 	"github.com/kalitsune/selector/structures"
@@ -15,18 +14,18 @@ func returnFileContent(ctx *fiber.Ctx, srv *drive.Service, fileid string) error 
 
 	list, err := api.IdToList(srv, fileid)
 	if err != nil {
+		if strings.HasPrefix(err.Error(), "googleapi: Error 404: File not found:") {
+			return fiber.ErrNotFound
+		}
 		return fiber.ErrInternalServerError
 	}
 
 	return ctx.JSON(list)
 }
 
-// GetList is a handler for the /api/lists route that returns a json containing
-// the random elements lists when providing its ID.
-// If no ID is provided, GetList will provide a list of all the lists
-func GetList(ctx *fiber.Ctx) error {
+// GetLists is a handler for the /api/lists route that returns an array of the lists
+func GetLists(ctx *fiber.Ctx) error {
 
-	id := ctx.Query("id")
 	token := ctx.Locals("token").(*oauth2.Token)
 
 	//get a Google Drive service
@@ -35,28 +34,38 @@ func GetList(ctx *fiber.Ctx) error {
 		return fiber.ErrInternalServerError
 	}
 
-	if id != "" {
-
-		return returnFileContent(ctx, srv, id)
-
-	} else {
-		//get the list of files
-		fileList, err := srv.Files.List().Spaces("appDataFolder").Do()
-		if err != nil {
-			return fiber.ErrInternalServerError
-		}
-
-		//convert the Google Drive files to lists
-		var listArray []structures.List
-		for _, file := range fileList.Files {
-			listArray = append(listArray, structures.List{Id: file.Id, Name: file.Name})
-		}
-
-		return ctx.JSON(listArray)
+	//get the list of files
+	fileList, err := srv.Files.List().Spaces("appDataFolder").Do()
+	if err != nil {
+		return fiber.ErrInternalServerError
 	}
+
+	//convert the Google Drive files to lists
+	var listArray []structures.List
+	for _, file := range fileList.Files {
+		listArray = append(listArray, structures.List{Id: file.Id, Name: file.Name})
+	}
+
+	return ctx.JSON(listArray)
 }
 
-// PostList is a handler for the /api/lists route that takes a json list object and
+// GetList is a handler for the /api/list/ route that returns a json containing
+// the random elements lists when providing its ID.
+func GetList(ctx *fiber.Ctx) error {
+
+	id := ctx.Params("id")
+	token := ctx.Locals("token").(*oauth2.Token)
+
+	//get a Google Drive service
+	srv, err := api.TokenToDriveClientService(token)
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
+	return returnFileContent(ctx, srv, id)
+}
+
+// PostList is a handler for the /api/list/ route that takes a json list object and
 // creates a new file in the user's Google Drive.
 func PostList(ctx *fiber.Ctx) error {
 
@@ -72,7 +81,7 @@ func PostList(ctx *fiber.Ctx) error {
 
 	// Create a list from the go type structures.List, parse the json body and handle potential errors
 	list := &structures.List{}
-	err = ctx.BodyParser(&list)
+	err = list.FromJSON(ctx.Body())
 	if err != nil {
 		return fiber.ErrBadRequest
 	}
@@ -81,9 +90,8 @@ func PostList(ctx *fiber.Ctx) error {
 	file := &drive.File{Name: list.Name, MimeType: "application/json", Parents: []string{"appDataFolder"}}
 
 	// Marshal the list to json
-	listData, err := json.Marshal(list)
+	listData, err := list.ToJSON()
 	if err != nil {
-		println("marshal error")
 		return fiber.ErrInternalServerError
 	}
 	// create a reader from the json data
@@ -92,19 +100,26 @@ func PostList(ctx *fiber.Ctx) error {
 	//save the file in the user's Google Drive and handle potential errors
 	file, err = srv.Files.Create(file).Media(reader).Do()
 	if err != nil {
-
 		return fiber.ErrInternalServerError
 	}
 
 	list.Id = file.Id
 
+	//update the file on Google Drive to give it its ID
+	reader = bytes.NewReader(listData)
+	_, err = srv.Files.Update(list.Id, &drive.File{}).Media(reader).Do()
+	if err != nil {
+		return fiber.ErrInternalServerError
+	}
+
 	return ctx.JSON(list)
 }
 
-// PatchList is a handler for the /api/lists route that takes a json list object and update it on the web
+// PatchList is a handler for the /api/list/ route that takes a json list object and update it on the web
 func PatchList(ctx *fiber.Ctx) error {
 	// get the token from the context
 	token := ctx.Locals("token").(*oauth2.Token)
+	id := ctx.Params("id")
 
 	//get a Google Drive service and handle potential server errors
 	srv, err := api.TokenToDriveClientService(token)
@@ -114,13 +129,14 @@ func PatchList(ctx *fiber.Ctx) error {
 
 	// Create a list from the go type structures.List, parse the json body and handle potential errors
 	list := &structures.List{}
-	err = ctx.BodyParser(&list)
+	err = list.FromJSON(ctx.Body())
 	if err != nil {
 		return fiber.ErrBadRequest
 	}
+	list.Id = id
 
 	// Marshal the list to json
-	listData, err := json.Marshal(list)
+	listData, err := list.ToJSON()
 	if err != nil {
 		return fiber.ErrInternalServerError
 	}
@@ -136,16 +152,11 @@ func PatchList(ctx *fiber.Ctx) error {
 	return ctx.JSON(list)
 }
 
-// DeleteList takes a list id and deletes it from the user's Google Drive
+// DeleteList is a handler for the /api/list/ route that takes a list id and deletes it from the user's Google Drive
 func DeleteList(ctx *fiber.Ctx) error {
 	// get the token from the context
 	token := ctx.Locals("token").(*oauth2.Token)
-	id := ctx.Query("id")
-
-	//checks if the ID is null
-	if id == "" {
-		return fiber.ErrBadRequest
-	}
+	id := ctx.Params("id")
 
 	//get a Google Drive service and handle potential server errors
 	srv, err := api.TokenToDriveClientService(token)
--- backend/structures/list.go
+++ backend/structures/list.go
@@ -1,13 +1,61 @@
 package structures
 
+import "encoding/json"
+
 type List struct {
-	Id       string         `json:"id"`
-	Name     string         `json:"name"`
-	Elements []RandomObject `json:"elements"`
+	Id       string           `json:"id"`
+	Name     string           `json:"name"`
+	Elements []RandomElements `json:"elements"`
 }
 
-type RandomObject struct {
+type RandomElements struct {
 	Name        string            `json:"name"`
 	Probability int               `json:"probability"`
 	Properties  map[string]string `json:"properties"`
+}
+
+// ToJSON returns the list as a json string
+func (list *List) ToJSON() ([]byte, error) {
+	return json.Marshal(list)
+}
+
+// FromJSON unmarshal a json into a List struct and correct malformed elements
+func (list *List) FromJSON(data []byte) error {
+	//unmarshal the data to a list
+	if err := json.Unmarshal(data, list); err != nil {
+		return err
+	}
+
+	//loop through each list element to check that every element is correctly formed
+	var correctedElements []RandomElements
+	for _, element := range list.Elements {
+
+		//check if the element has a name
+		if element.Name == "" {
+			element.Name = "Unnamed element"
+		}
+
+		// check if the probability is between 0 and 100
+		if element.Probability < 0 || element.Probability > 100 {
+			element.Probability = 0
+		}
+
+		//check if the properties are correctly formed (not nil)
+		if element.Properties == nil {
+			element.Properties = make(map[string]string)
+		}
+
+		//add the element to the corrected elements array
+		correctedElements = append(correctedElements, element)
+	}
+
+	//set the corrected elements to the list
+	list.Elements = correctedElements
+
+	//check if elements is nil
+	if list.Elements == nil {
+		list.Elements = []RandomElements{}
+	}
+
+	return nil
 }
