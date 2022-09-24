2a4a186f52f151c436e1f17717ad12a31b771251
Kali <kalithekitsune@gmail.com>
2022-09-24 23:33:23 +0200

    fixed vite proxy

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,7 +2,6 @@
   <div class="-z-50 fixed bg-neutral-100 dark:bg-neutral-800 w-screen h-screen">
     <App v-if="this.$cookies.isKey('token')"/>
     <Welcome v-else/>
-<!--    {{ this.$cookies.set('token', "test") }}-->
   </div>
 </template>
 
--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  devServer: {
-    proxy: 'http://localhost:8080'
+  server: {
+    proxy: {
+      '/auth': 'http://127.0.0.1:8080',
+      '/api': 'http://127.0.0.1:8080',
+    }
   },
   plugins: [vue()]
 })
