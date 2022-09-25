7ebac4a6115f83b2cfdda0a777adbf82fab1d5c3
Kali <kalithekitsune@gmail.com>
2022-09-25 17:13:55 +0200

    removed unnecessary code

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import vueCookies from 'vue-cookies'
-import { Vue3Mq } from 'vue3-mq'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
@@ -15,18 +14,6 @@ let app = createApp(index)
 
 //use vue-cookies to check the presence of some cookies
 app.use(vueCookies)
-
-//use vue-mq to enable responsive design and alternative components (see https://www.digitalocean.com/community/tutorials/vuejs-vue-media-queries)
-app.use(Vue3Mq, {
-    breakpoints: {
-        "sm": 640,
-        "md": 768,
-        "lg": 1024,
-        "xl": 1280,
-        "xxl": 1536,
-    }
-})
-
 
 app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
